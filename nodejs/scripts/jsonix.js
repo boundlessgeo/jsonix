d605e1576c4e1711453e3cd934d310caad9a9d52
bartvde <bartvde@boundlessgeo.com>
2017-05-30 14:03:57 +0200

    Fix up test failures

--- nodejs/scripts/jsonix.js
+++ nodejs/scripts/jsonix.js
@@ -6106,11 +6106,9 @@ Jsonix.Context = Jsonix
 if (typeof require === 'function') {
 	// ... but the define function does not exists
 	if (typeof define !== 'function') {
-		// Load the define function via amdefine
-		var define = require('amdefine')(module);
-		// If we're not in browser
-		if (typeof window === 'undefined')
-		{
+		if (!process.browser) {
+			// Load the define function via amdefine
+			define = require('amdefine')(module);
 			// Require xmldom, xmlhttprequest and fs
 			define(["xmldom", "xmlhttprequest", "fs"], _jsonix_factory);
 		}
@@ -6134,3 +6132,4 @@ else
 	// Just call the factory and set Jsonix as global.
 	var Jsonix = _jsonix_factory().Jsonix;
 }
+
